41443f57f6a638138be113fbd726ffc6577330a1
Koni99 <kstroemel@uos.de>
2021-10-06 11:04:49 +0200

    evaluation baseline
    changed description of label frequency classifier

--- code/classification/run_classifier.py
+++ code/classification/run_classifier.py
@@ -20,7 +20,7 @@ parser.add_argument("-e", "--export_file", help = "export the trained classifier
 parser.add_argument("-i", "--import_file", help = "import a trained classifier from the given location", default = None)
 parser.add_argument("-m", "--majority", action = "store_true", help = "majority class classifier")
 parser.add_argument("-u", "--uniform", action = "store_true", help = "uniform distribution classifier")
-parser.add_argument("-s", "--stratified", action = "store_true", help = "random predictions respecting traning set class distributions")
+parser.add_argument("-l", "--label", action = "store_true", help = "label frequency classfier")
 parser.add_argument("-a", "--accuracy", action = "store_true", help = "evaluate using accuracy")
 parser.add_argument("-auc", "--area", action = "store_true", help = "evaluate using area under curve")
 parser.add_argument("-c", "--cohen", action = "store_true", help = "evaluate using cohen kappa score")
@@ -48,8 +48,8 @@ else:   # manually set up a classifier
         classifier = DummyClassifier(strategy = "uniform", random_state = args.seed)
         classifier.fit(data["features"], data["labels"])
     elif args.stratified:
-        # uniform classifier
-        print("    random predictions respecting traning set class distributions")
+        # label frequency classifier
+        print("    random predictions respecting traning set class distributions (label frequency)")
         classifier = DummyClassifier(strategy = "stratified", random_state = args.seed)
         classifier.fit(data["features"], data["labels"])
         
@@ -74,3 +74,4 @@ for metric_name, metric in evaluation_metrics:
 if args.export_file is not None:
     with open(args.export_file, 'wb') as f_out:
         pickle.dump(classifier, f_out)
+        
