68e32a321ece9a5abdbf9b69b5358c6360a9acb2
marlens123 <marlena@itdex.de>
2021-10-28 14:36:50 +0200

    modified hashtag_counter
    modified input column so that it is correct

--- code/feature_extraction/hashtag_counter.py
+++ code/feature_extraction/hashtag_counter.py
@@ -7,12 +7,11 @@ Created on Fri Oct  8 11:45:03 2021
 @author: kstroemel
 """
 from code.feature_extraction.feature_extractor import FeatureExtractor
-from code.util import COLUMN_HASHTAGS
 
 class HashtagCounter(FeatureExtractor):
     #constructor
     def __init__(self, input_column):
-        super().__init__([COLUMN_HASHTAGS],"{0}_number_of_hashtags".format(input_column))
+        super().__init__([input_column],"{0}_number_of_hashtags".format(input_column))
 
     def _get_values(self, inputs):
         """Count number of hashtags."""
